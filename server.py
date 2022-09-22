09c06764ac83734f73d62559367f1c9264b771f8
xu-kai-xu <xuk1111@foxmail.com>
2022-09-22 15:44:36 +0800

    add wsgi support

--- server.py
+++ server.py
@@ -12,7 +12,8 @@ class index:
         data = web.input(name=None)
         return render.index(data.name)  # render 的方法 index 与模板文件夹下的模板文件需要同名
 
+app = web.application(urls, globals())
+wsgiapp = app.wsgifunc()
 
 if __name__ == '__main__':
-    app = web.application(urls, globals())
     app.run() 
